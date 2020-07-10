930bbacae2bf84bc9cb3b961a4a6b8872bcf2ddb
jmarinllao <josepmarinllao@gmail.com>
2020-07-10 14:23:13 +0200

    Dataset3 update

--- scripts/on_kegg/diffupath_validation.sh
+++ scripts/on_kegg/diffupath_validation.sh
@@ -12,9 +12,9 @@ results_dir=$(dirname $(dirname $(dirname $(realpath $0))))
 pkg_path=${HOME}/.diffupath
 
 python3 -m diffupath diffusion evaluate \
-  --comparison 'method' \
+  --comparison 'database' \
   --data_path ${results_dir}/data/input_mappings \
-  --graph ${results_dir}/data/pickles/databases/graph_pathme_kegg.pickle \
+  --graph ${results_dir}/data/pickles/universe/pathme_universe_non_flatten_collapsed_names_no_isolates_16_03_2020.pickle \
   --kernel ${pkg_path}/kernels/kernel_regularized_kegg.pickle \
   --output ${results_dir}/outputs/evaluation_metrics.json \
-  --iterations 10
+  --iterations 3
